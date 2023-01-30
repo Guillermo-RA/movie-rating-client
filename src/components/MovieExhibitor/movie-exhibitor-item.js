d8c123c3fe86727f95147ce72fea9dc4f0670d88
GuilleFive <romagu.guillermo@gmail.com>
2023-01-30 14:49:27 +0100

    Sacar aparte el averageVote con el redondeo hecho

--- src/components/MovieExhibitor/movie-exhibitor-item.js
+++ src/components/MovieExhibitor/movie-exhibitor-item.js
@@ -16,7 +16,7 @@ const MovieExhibitorItem = ({
                             }) => {
 
     const {tmdbImageUrl} = useContext(ApiDataProvider)
-
+    const averageVote = Math.round((vote_average + Number.EPSILON) * 100) / 100;
     return (
         <Link to={`/movies/${id}`}>
             <div className="movie-card">
@@ -28,7 +28,7 @@ const MovieExhibitorItem = ({
                     </div>
                     <div className="movie-card-back">
                         <p className="movie-card-title">{title || name}</p>
-                        <div className='movie-card-subtitle'><Star width='20' color='#FFCC6A'/><span>{Math.round((vote_average + Number.EPSILON) * 100) / 100}/10</span></div>
+                        <div className='movie-card-subtitle'><Star width='20' color='#FFCC6A'/><span>{averageVote}/10</span></div>
                         <div className='w-100'>
                             <img alt={title} className='movie-card-image' src={`${tmdbImageUrl}${poster_path}`}/>
                         </div>
